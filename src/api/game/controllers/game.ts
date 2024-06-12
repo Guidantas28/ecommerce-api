4daef0c844c8f7cbfa3b5c76595e01e9050c21a1
Guilherme Dantas <guilhermedantas788@gmail.com>
2024-06-12 19:08:36 -0300

    finished populate data and finished the api

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -6,10 +6,14 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::game.game', ({strapi}) => ({
   async populate(ctx){
-    console.log("RODANDO NO SERVIDOR")
+    const options = {
+      limit: 48,
+      order: "desc:trending",
+      ...ctx.query,
+    };
 
-    await strapi.service("api::game.game").populate(ctx.query)
+    await strapi.service("api::game.game").populate(options);
 
-    ctx.send("Finalizado no client")
+    ctx.send("Finished populating games!");
   }
 }));
