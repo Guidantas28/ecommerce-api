15abdd01ca481bffbe78eb322fb23b70ca921fe0
Guilherme Dantas <guilhermedantas788@gmail.com>
2024-06-12 17:27:41 -0300

    created structure many to many

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -5,6 +5,13 @@
 import axios from "axios"
 import { JSDOM } from "jsdom"
 import { factories } from '@strapi/strapi';
+import slugify from "slugify";
+
+const gameService = "api::game.game";
+const publisherService = "api::publisher.publisher";
+const developerService = "api::developer.developer";
+const categoryService = "api::category.category";
+const platformService = "api::platform.platform";
 
 async function getGameInfo(slug){
   const gogSlug = slug.replaceAll('-', '_').toLowerCase();
@@ -29,6 +36,107 @@ async function getGameInfo(slug){
 
 }
 
+async function getByName(name, entityService){
+  const item = await strapi.service(entityService).find({
+    filters: { name },
+  })
+
+  return item.results.length > 0 ? item.results[0] : null
+}
+
+async function create(name, entityService){
+  const item = await getByName(name, entityService)
+
+  if(!item) {
+    await strapi.service(entityService).create({
+      data: {
+        name,
+        slug: slugify(name, {strict: true, lower: true})
+      }
+    })
+  }
+}
+
+async function createManyToManyData(products) {
+  const developersSet = new Set();
+  const publishersSet = new Set();
+  const categoriesSet = new Set();
+  const platformsSet = new Set();
+
+  products.forEach((product) => {
+    const { developers, publishers, genres, operatingSystems } = product;
+
+    genres?.forEach(({ name }) => {
+      categoriesSet.add(name);
+    });
+
+    operatingSystems?.forEach((item) => {
+      platformsSet.add(item);
+    });
+
+    developers?.forEach((item) => {
+      developersSet.add(item);
+    });
+
+    publishers?.forEach((item) => {
+      publishersSet.add(item);
+    });
+  });
+
+  const createCall = (set, entityName) =>
+    Array.from(set).map((name) => create(name, entityName));
+
+  return Promise.all([
+    ...createCall(developersSet, developerService),
+    ...createCall(publishersSet, publisherService),
+    ...createCall(categoriesSet, categoryService),
+    ...createCall(platformsSet, platformService),
+  ]);
+}
+
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, gameService);
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.service(`${gameService}`).create({
+          data: {
+            name: product.title,
+            slug: product.slug,
+            price: product.price.finalMoney.amount,
+            release_date: new Date(product.releaseDate),
+            categories: await Promise.all(
+              product.genres.map(({ name }) => getByName(name, categoryService))
+            ),
+            platforms: await Promise.all(
+              product.operatingSystems.map((name) =>
+                getByName(name, platformService)
+              )
+            ),
+            developers: await Promise.all(
+              product.developers.map((name) =>
+                getByName(name, developerService)
+              )
+            ),
+            publisher: await Promise.all(
+              product.publishers.map((name) =>
+                getByName(name, publisherService)
+              )
+            ),
+            ...(await getGameInfo(product.slug)),
+            publishedAt: new Date(),
+          },
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
     const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`
@@ -37,6 +145,9 @@ export default factories.createCoreService('api::game.game', () => ({
       data: {products},
     } = await axios.get(gogApiUrl)
 
-    console.log(await getGameInfo(products[2].slug));
+    await createManyToManyData([products[0], products[2]]);
+
+    await createGames([products[0], products[2]]);
+
     }
 }));
